73c411c0351ed59f24f4e342469668bbd1985883
Mikhail Strizhov <m.strizhov@cyprus.ru>
2019-12-05 20:18:14 +0300

    Edit / view order

--- web/admin/react/src/components/pages/order/Order.jsx
+++ web/admin/react/src/components/pages/order/Order.jsx
@@ -2,68 +2,8 @@ import { Form, Input, Button } from 'antd';
 import React, { Component } from 'react';
 import {getOrder} from "../../../actions";
 import {connect} from "react-redux";
-import {Formik, withFormik} from "formik";
+import {Formik, Field} from "formik";
 import * as Yup from "yup";
-
-const OrderForm = props => {
-	const {
-		onEditEnable,
-		onEditDisable,
-		editable,
-		values,
-		touched,
-		errors,
-		handleChange,
-		handleBlur,
-		handleSubmit,
-	} = props;
-	return (
-		<div className="form">
-			<React.Fragment>
-				<div className='form-head'>
-					<div className='form-title'>
-						Order
-					</div>
-					<div className='form-space_between'>&nbsp;</div>
-					{!editable &&
-					<div className='form-toolbar'>
-						<Button type="primary" onClick={onEditEnable}>Edit</Button>
-					</div>
-					}
-				</div>
-				<div className='form-content'>
-					<Form.Item label="Name">
-						<Input name='name' onChange={handleChange} onBlur={handleBlur} value={values.name} disabled={!editable} />
-					</Form.Item>
-					<Form.Item label="Phone">
-						<Input name='phone' onChange={handleChange} onBlur={handleBlur} value={values.phone} disabled={!editable} />
-					</Form.Item>
-					<Form.Item label="Address">
-						<Input.TextArea name='address' rows={5} onChange={handleChange} onBlur={handleBlur} value={values.address} disabled={!editable} />
-					</Form.Item>
-					<Form.Item label="Client's comment">
-						<Input.TextArea name='comment' rows={5} onChange={handleChange} onBlur={handleBlur} value={values.comment} disabled={!editable} />
-					</Form.Item>
-					<Form.Item label="Our comment">
-						<Input.TextArea name='comment' rows={5} onChange={handleChange} onBlur={handleBlur} value={values.comment} disabled={!editable} />
-					</Form.Item>
-				</div>
-				{editable &&
-				<div className='form-bottom'>
-					<Button type="primary">Submit</Button>
-					<Button onClick={onEditDisable} style={{ marginLeft: '10px' }}>Cancel</Button>
-				</div>
-				}
-			</React.Fragment>
-		</div>
-	);
-};
-
-const OrderFormikForm = withFormik({
-	mapPropsToValues: props => {
-		return (props.order);
-	}
-})(OrderForm);
 
 class Orders extends Component {
 	constructor(props) {
@@ -98,13 +38,53 @@ class Orders extends Component {
               console.log(this.props.order.items);
           }
 
+          const editable = this.state.editable;
+          const onEditEnable = this.enableEdit;
+					const onEditDisable = this.disableEdit;
+
           return (
           	<React.Fragment>
 						{ this.props.order &&
-              <OrderFormikForm
-                  order={{name: 'name1', address: 'address1', phone: '1234567', comment: 'comment1'}}
-									onEditEnable={this.enableEdit} onEditDisable={this.disableEdit}
-							/>
+							<Formik initialValues={{name: 'name2', address: 'address2', phone: '12345678', comment: 'comment2'}}>
+								<div className="form">
+									<React.Fragment>
+										<div className='form-head'>
+											<div className='form-title'>
+												Order
+											</div>
+											<div className='form-space_between'>&nbsp;</div>
+											{!editable &&
+											<div className='form-toolbar'>
+												<Button type="primary" onClick={onEditEnable}>Edit</Button>
+											</div>
+											}
+										</div>
+										<div className='form-content'>
+											<Form.Item label="Name">
+												<Field as={Input} name='name' disabled={!editable} />
+											</Form.Item>
+											<Form.Item label="Phone">
+												<Field as={Input} name='phone' disabled={!editable} />
+											</Form.Item>
+											<Form.Item label="Address">
+												<Field as={Input.TextArea} name='address' rows={5} disabled={!editable} />
+											</Form.Item>
+											<Form.Item label="Client's comment">
+												<Field as={Input.TextArea} name='comment' rows={5} disabled={!editable} />
+											</Form.Item>
+											<Form.Item label="Our comment">
+												<Field as={Input.TextArea} name='comment' rows={5} disabled={!editable} />
+											</Form.Item>
+										</div>
+										{editable &&
+										<div className='form-bottom'>
+											<Button type="primary">Submit</Button>
+											<Button onClick={onEditDisable} style={{ marginLeft: '10px' }}>Cancel</Button>
+										</div>
+										}
+									</React.Fragment>
+								</div>
+							</Formik>
 						}
 						</React.Fragment>
           );
